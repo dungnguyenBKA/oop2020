76c087d2fa7b0a678a37fb676137731b5b5532d2
dungnguyenBKA <dungdasua365@gmail.com>
2021-03-28 21:49:08 +0700

    update lab 5

---
+++ lab05/src/NoGarbage.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class NoGarbage {
+    public static void main(String[] args) {
+        Random r= new Random(1000);
+        long start = System.currentTimeMillis();
+        StringBuilder s = new StringBuilder();
+        for(int i= 0; i<200000; i++)
+            s.append(r.nextInt(2));
+        //s.toString();
+        System.out.println(System.currentTimeMillis()-start);
+    }
+}
+
+
